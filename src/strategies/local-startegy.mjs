ace45849401505367d282cb48bb98907668fb4ff
Peter Migichi <petermigichi47@gmail.com>
2026-07-31 03:38:35 +0300

    initial commit of express tutorial

---
+++ src/strategies/local-startegy.mjs
@@ -0,0 +1,24 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+import { mockUsers } from "../utils/constants.mjs";
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+export default passport.use(
+  new Strategy((username, password, done) => {
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
+
+    try {
+      const findUser = mockUsers.find((user) => user.username === username);
+      if (!findUser) throw new Error("user not found");
+      if (findUser.password !== password)
+        throw new Error("Invalid credentials");
+      done(null, findUser);
+    } catch (err) {
+      done(err, null);
+    }
+  }),
+);
