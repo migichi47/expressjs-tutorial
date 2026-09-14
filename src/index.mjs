209dcec3f35b600aeded3e5a4ee8f6398f1b4aa6
Peter Migichi <petermigichi47@gmail.com>
2026-09-14 07:13:08 +0300

    .

--- src/index.mjs
+++ src/index.mjs
@@ -3,8 +3,10 @@ import cookieParser from "cookie-parser";
 import passport from "passport";
 import session from "express-session";
 import mongoose from "mongoose";
-import "./routes/strategies/local-strategy.mjs";
+import MongoStore from "connect-mongo";
 import routes from "./routes/index.mjs";
+// import "./strategies/local-strategy.mjs";
+import './strategies/discord-strategies.mjs'
 
 const app = express();
 
@@ -23,6 +25,9 @@ app.use(
     cookie: {
       maxAge: 60000 * 60,
     },
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+    }),
   }),
 );
 app.use(passport.initialize());
@@ -47,6 +52,11 @@ app.post("/api/auth/logout", (req, res) => {
   });
 });
 
+app.get('/api/auth/discord', passport.authenticate('discord'))
+app.get('/api/auth/discord/redirect', passport.authenticate('discord'), (req, res) => {
+  res.sendStatus(200)
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
@@ -59,3 +69,9 @@ app.get("/", (request, response) => {
   response.cookie("hello", "world", { maxAge: 30000, signed: true });
   response.status(201).send({ msg: "Hello" });
 });
+
+
+
+// client_secret = bAy9phvq5XFO7_OW1U4i4NBYhN4mpYJs
+//  client_id = 1546715275997483048
+// redirect = http://localhost:3000/api/auth/discord/redirect
---
+++ src/strategies/discord-strategies.mjs
@@ -0,0 +1,43 @@
+import passport from "passport";
+import { Strategy } from "passport-discord";
+import { DiscordUser } from "../mongoose/schemas/discord-user.mjs";
+
+passport.serializeUser((user, done) => {
+  console.log("inside Serialize user");
+  console.log(user);
+  done(null, user.id);
+});
+
+export default passport.use(
+  new Strategy(
+    {
+      clientID: "1546715275997483048",
+      clientSecret: "bAy9phvq5XFO7_OW1U4i4NBYhN4mpYJs",
+      callbackURL: "http://localhost:3000/api/auth/discord/redirect",
+      scope: ["identify", "guilds"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      let findUser;
+
+      try {
+        findUser = await DiscordUser.findOne({ discordId: profile.id });
+      } catch (err) {
+        return done(err, null);
+      }
+
+      try {
+        if (!findUser) {
+          const newUser = new DiscordUser({
+            username: profile.username,
+            discordId: profile.id,
+          });
+          const newSavedUser = await newUser.save();
+          return done(null, newSavedUser);
+        }
+        return done(null, findUser)
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  ),
+);
